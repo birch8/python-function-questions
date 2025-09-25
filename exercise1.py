d5b091cc0ae70a721b0140d910c4872c35164671
birch8 <210781416+birch8@users.noreply.github.com>
2025-09-25 10:15:04 +1000

    Update exercise1.py

---
+++ exercise1.py
@@ -0,0 +1,3 @@
+# Write a Python function that takes a name as input and returns a personalised greeting.
+def greeting(name): return f"Hello there, {name}!"
+print(greeting(input("Enter your name: ")))
