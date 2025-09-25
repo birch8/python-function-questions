99189784dc6a2ac43da60d7a4173bb3c4e9ac5d8
birch8 <210781416+birch8@users.noreply.github.com>
2025-09-25 10:42:33 +1000

    Update exercise4.py

--- exercise4.py
+++ exercise4.py
@@ -1,1 +1,8 @@
 # Write a Python function that takes a sentence and returns the number of vowels in it (ignore case).
+def num_vowels(sentence):
+    vowels = ["a", "e", "i", "o", "u"]; num_vowels = 0
+    for letter in sentence.lower():
+        if letter in vowels:
+            num_vowels += 1
+    return num_vowels
+print(num_vowels("fOur vowEls"))
