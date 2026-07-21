ce46a5d2df7abffdae4690f4eaa2bc8603fba123
emby01 <210781416+birch8@users.noreply.github.com>
2026-07-21 12:23:32 +1000

    Bubble

--- exercise5.py
+++ exercise5.py
@@ -1,2 +1,4 @@
 # Write a function that sorts a list of numbers using bubble sort (do not use Python's built-in sort).  
 # You will need to check on how the bubble sort algorithm works first.
+def bubble_sort(list):
+    [i for i in range(1, 11)] # Get sorted list of numbers
